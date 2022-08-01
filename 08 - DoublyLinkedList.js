2e624fdbac135d2bbf75934caaf36b63b515451a
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-08-01 23:09:57 +0530

    DoublyLinkedList push

---
+++ 08 - DoublyLinkedList.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+}
+
+const linkedList = new DoublyLinkedList()
+
+console.log(linkedList)
+
+linkedList.push(12)
+
+console.log(linkedList)
+
+linkedList.push(23)
+
+console.log(linkedList)
