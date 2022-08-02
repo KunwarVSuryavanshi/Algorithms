25f0aab41e68fd40a99dd1197f70ab31141e954a
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-08-03 00:08:11 +0530

    pop, shift and unshift

--- 08 - DoublyLinkedList.js
+++ 08 - DoublyLinkedList.js
@@ -28,6 +28,57 @@ class DoublyLinkedList {
 
     return this;
   }
+
+  pop() {
+    if (this.length === 0) return
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return this;
+    }
+    let prevObj = this.tail.prev;
+    prevObj.next = null;
+    this.tail = prevObj;
+    this.length--;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) return;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return this;
+    }
+
+    let nextNode = this.head.next;
+    nextNode.prev = null;
+    this.head = nextNode;
+    this.length--;
+    return this;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length = 1;
+      return this;
+    }
+
+    let oldHead = this.head;
+    
+    this.head = newNode;
+    this.head.next = oldHead
+    oldHead.prev = this.head // Dont forget setting prev null to current head
+    this.length++;
+    
+    return this;
+  }
 }
 
 const linkedList = new DoublyLinkedList()
@@ -39,5 +90,21 @@ linkedList.push(12)
 console.log(linkedList)
 
 linkedList.push(23)
+
+console.log(linkedList)
+
+linkedList.push(69)
+
+console.log(linkedList)
+
+linkedList.pop()
+
+console.log(linkedList)
+
+linkedList.shift()
+
+console.log(linkedList)
+
+linkedList.unshift(79)
 
 console.log(linkedList)
